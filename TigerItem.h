80f119e2f317afff2ab713803835770c9242e3b0
RicardoG1995 <r.gomez@udea.edu.co>
2025-07-09 21:22:22 -0500

    ataque y monitoreo enemigo   TigerItem.h
    Se agrega función para eliminar al enemigo y monitoreo de este

--- TigerItem.h
+++ TigerItem.h
@@ -12,6 +12,7 @@ class TigerItem : public QObject, public QGraphicsPixmapItem {
 public:
     TigerItem();  // Constructor que inicializa el movimiento
     void moveOscillate(); // Movimiento en onda senoidal
+    void setAlive(bool alive);  // Permite eliminar visualmente al tigre
 
 private:
     QPixmap normalPixmap;      // Imagen normal del tigre
@@ -20,6 +21,7 @@ private:
     int direction;             // Dirección horizontal (opcional para más lógica)
     qreal amplitude;           // Amplitud de oscilación (cuánto se mueve a los lados)
     qreal baseX;               // Posición base (centro) del movimiento
+    bool isAlive;              // Estado del enemigo
 };
 
 #endif // TIGERITEM_H
